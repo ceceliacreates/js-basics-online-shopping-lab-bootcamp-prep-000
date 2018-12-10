a68d9cdef379ce84d64ac6c84c65c5626ae1c093
Cecelia Smith <ceceliacreates@gmail.com>
2018-12-10 21:34:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart.push({itemName: item, itemPrice: (Math.floor(Math.random() * 100))})
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var outputString = "In your cart, you have"
+  var i;
+  if (cart.length === 0) {
+      outputString = "Your shopping cart is empty."
+    }
+  else {
+  for (i = 0; i < cart.length; i++) {
+  if (cart.length === 1) {
+      outputString += `${cart.itemName[0]} at ${cart.itemPrice[0]}.`
+    }
+    else if (i === cart.length) {
+      outputString += `, and ${cart.itemName[i]} at ${cart.itemPrice[i]}.`
+    }
+    else {
+      outputString += `,${cart.itemName[i]} at ${cart.itemPrice[i]}`
+    }
+  }
+  }
+  return outputString
 }
 
 function total() {
